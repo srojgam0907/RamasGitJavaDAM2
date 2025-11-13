113b44b0bd346180dda4f92b1fa39b08b26a15c6
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-13 10:39:24 +0100

    primer commit con estructura de loteria

---
+++ src/edu/alumno/ramas/Main.java
@@ -0,0 +1,18 @@
+package edu.alumno.ramas;
+
+import java.util.Random;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        // Lotería Primitiva: 5 números principales (1..49) + 1 complementario
+        HashSet<Integer> bolas = new HashSet<>();
+        Random r = new Random();
+        while (bolas.size() < 5) {
+            bolas.add(r.nextInt(49) + 1);
+        }
+        System.out.println("Números principales: " + bolas);
+        int complementario = r.nextInt(49) + 1;
+        System.out.println("Complementario: " + complementario);
+    }
+}
